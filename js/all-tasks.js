0e68e1889ad673829feb09ead73b5efac1651915
Anna Nikitina <hansa8805@gmail.com>
2021-01-29 00:50:18 +0300

    Исправила задачи js

---
+++ js/all-tasks.js
@@ -0,0 +1,24 @@
+import users from "./users.js"
+
+/*===== Задача 1 =====*/
+// Получить массив имен всех пользователей (поле name).
+
+
+const getUserNames = users => {
+  const userNames = users.map(user => user.name);
+  return userNames;
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+/*===== Задача 2 =====*/
+/*===== Задача 3 =====*/
+/*===== Задача 4 =====*/
+/*===== Задача 5 =====*/
+/*===== Задача 6 =====*/
+/*===== Задача 7 =====*/
+/*===== Задача 8 =====*/
+/*===== Задача 9 =====*/
+/*===== Задача 10 =====*/
--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-// Получить массив имен всех пользователей (поле name).
-
-
-const getUserNames = users => {
-  getUserNames.forEach(name => console.log(name));
-};
-
-console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
